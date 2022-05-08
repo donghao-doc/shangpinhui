a60841b64a8fd8ed6a63e4aae854dd1d6361550b
BarryDong <286084845@qq.com>
2022-05-08 20:43:37 +0800

    调接口获取三级联动数据 & 三级联动动态显示

--- src/store/home.js
+++ src/store/home.js
@@ -1,7 +1,27 @@
+import { reqCategoryList } from '@/api'
+
 export default {
     namespaced: true,
-    state: {},
+    state: {
+        categoryList: null
+    },
     getters: {},
-    mutations: {},
-    actions: {}
+    mutations: {
+        SETCATEGORYLIST(state, value) {
+            state.categoryList = value
+        }
+    },
+    actions: {
+        async getCategoryList(context) {
+            try {
+                const result = await reqCategoryList()
+                console.log('getCategoryList:', result)
+                if (result.code === 200) {
+                    context.commit('SETCATEGORYLIST', result.data)
+                }
+            } catch (err) {
+                console.log('getCategoryList err:', err)
+            }
+        }
+    }
 }

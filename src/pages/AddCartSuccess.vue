f4508664b27a13dc15ddad529686f94e158cfa9d
BarryDong <286084845@qq.com>
2022-05-16 13:30:58 +0800

    加入购物车吹成功页面点击按钮跳转

--- src/pages/AddCartSuccess.vue
+++ src/pages/AddCartSuccess.vue
@@ -13,8 +13,8 @@
           </div>
         </div>
         <div class="right-gocart">
-          <a href="javascript:" class="sui-btn btn-xlarge">查看商品详情</a>
-          <a href="javascript:">去购物车结算 > </a>
+          <router-link :to="`/detail/${skuInfo.id}`" class="sui-btn btn-xlarge">查看商品详情</router-link>
+          <router-link to="/shopCart">去购物车结算 > </router-link>
         </div>
       </div>
     </div>
